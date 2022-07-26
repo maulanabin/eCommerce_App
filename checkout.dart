e6123b00f8468c2a94f5e23af4d3f03ebed552de
maulanabin <2041720132@student.polinema.ac.id>
2022-07-26 17:03:34 +0700

    Menambahkan checkout function

--- checkout.dart
+++ checkout.dart
@@ -31,6 +31,8 @@ class Cart {
       _items[product.id] = Item(product: product, quantity: item.quantity + 1);
     }
   }
+  bool get isEmpty => _items.isEmpty;
+
   double total() => _items.values
         .map((item) => item.price)
         .reduce((value, element) => value + element);
@@ -81,7 +83,9 @@ void main() {
     } else if (line == 'v') {
       print(cart);
     } else if (line == 'c') {
-      // TODO: IMPLEMENT
+      if (checkout(cart)) {
+        break;
+      }
     }
   }
 }
@@ -99,4 +103,31 @@ Product? chooseProduct() {
   }
   print('Not found');
   return null;
+}
+
+bool checkout(Cart cart) {
+  if (cart.isEmpty) {
+    print('Cart is empty');
+    return false;
+  }
+  final total = cart.total();
+  print('Total: \$$total');
+  stdout.write('Payment in cash: ');
+  final line = stdin.readLineSync();
+
+  if (line == null || line.isEmpty) {
+    return false;
+  }
+  final paid = double.tryParse(line);
+  if (paid == null) {
+    return false;
+  }
+  if (paid >= total) {
+    final change = paid - total;
+    print('Change: \$${change.toStringAsFixed(2)}');
+    return true;
+  } else {
+    print('Not enough cash.');
+    return false;
+  }
 }
